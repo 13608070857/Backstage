bc108ce2ffa663c2bbe5636ed4bc6998c31eef3d
jysson <jysson1224@users.noreply.github.com>
2018-07-17 17:23:40 +0800

    s

---
+++ myvue/src/components/search.vue
@@ -0,0 +1,35 @@
+<template>
+  <div id="search">
+    <input type="text">
+    <btn btnText="查询" btnClass="lightGreen large search"></btn>
+    <btn btnText="新增" btnClass="darkGreen large add"></btn>
+  </div>
+</template>
+
+<script>
+import btn from './button/button'
+export default {
+  name: 'search',
+  components: {
+    btn
+  }
+}
+</script>
+
+<style scoped>
+.add {
+  margin-left: 30px;
+}
+input {
+  border: 1px solid #439B66;
+  padding: 10px 5px;
+  width: 240px;
+  padding-top: 11px;
+}
+input:focus {
+  outline: none;
+}
+.search {
+  margin-left: -8px;
+}
+</style>
