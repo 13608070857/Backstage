13e0f585aa6e18c669fe7b4150fa1bd197d223fc
Xiexinming <30278740+Xiexinming@users.noreply.github.com>
2018-07-19 14:52:55 +0800

    123

--- myvue/src/components/index/index.vue
+++ myvue/src/components/index/index.vue
@@ -7,14 +7,22 @@
       <p>{{i.ZTitle}}:</p>
       <p>{{i.ZNumber}}ZNumber</p>
     </article>
+    <div id="line" style="width: 750px;height: 400px;float: left;margin-top: 30px"></div>
   </div>
 </template>
 
 <script>
+let echarts = require('echarts/lib/echarts')
+// 引入柱状图
+require('echarts/lib/chart/line')
+// 引入提示框和标题组件
+require('echarts/lib/component/tooltip')
+require('echarts/lib/component/title')
 export default {
   name: 'index',
   data () {
     return {
+      // 数据
       arr: [
         {
           Title: '商城用户',
@@ -49,6 +57,71 @@ export default {
           background: '#FFB800'
         }
       ]
+    }
+  },
+  mounted () {
+    this.Line()
+  },
+  methods: {
+    // 折线图
+    Line () {
+      let line = echarts.init(document.getElementById('line'))
+      line.setOption({
+        title: {
+          text: '  商城信息图'
+        },
+        tooltip: {
+          trigger: 'axis'
+        },
+        legend: {
+          data: ['商城用户', '售后记录', '交易记录', '商城订单']
+        },
+        grid: {
+          left: '3%',
+          right: '4%',
+          bottom: '3%',
+          containLabel: true
+        },
+        toolbox: {
+          feature: {
+            saveAsImage: {}
+          }
+        },
+        xAxis: {
+          type: 'category',
+          boundaryGap: false,
+          data: ['一月', '二月', '三月', '四月', '五月', '六月', '七月', '八月', '九月', '十月', '十一月', '十二月']
+        },
+        yAxis: {
+          type: 'value'
+        },
+        series: [
+          {
+            name: '商城用户',
+            type: 'line',
+            stack: '总量',
+            data: [120, 200, 256, 278, 321, 360, 454, 660, 684, 750, 799, 856]
+          },
+          {
+            name: '售后记录',
+            type: 'line',
+            stack: '总量1',
+            data: [20, 30, 33, 46, 31, 19, 46, 39, 35, 29, 25, 19]
+          },
+          {
+            name: '交易记录',
+            type: 'line',
+            stack: '总量2',
+            data: [99, 150, 256, 198, 245, 290, 277, 311, 350, 322, 369, 377]
+          },
+          {
+            name: '商城订单',
+            type: 'line',
+            stack: '总量3',
+            data: [150, 232, 250, 335, 359, 424, 473, 529, 577, 610, 653, 680]
+          }
+        ]
+      })
     }
   }
 }
