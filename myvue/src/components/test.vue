5424fee591a3ac52e6d691da5fbf9a02d645d170
jysson <jysson1224@users.noreply.github.com>
2018-07-19 09:04:34 +0800

    s

--- myvue/src/components/test.vue
+++ myvue/src/components/test.vue
@@ -1,13 +1,13 @@
 <template>
-  <div id="app">
-    <appTable></appTable>
+  <div id="test">
+    <appTable :tableTitles="tableTitles" :router="router"></appTable>
   </div>
 </template>
 
 <script>
 import appTable from './table.vue'
 export default {
-  name: 'App',
+  name: 'Test',
   data () {
     return {
       tableTitles: ['测试id', '测试名称', '测试的其他数据'],
