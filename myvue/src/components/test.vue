6806bc1c872fe9c82dab8e6246d3ff54c413a88e
jysson <jysson1224@users.noreply.github.com>
2018-07-19 08:44:42 +0800

    s

---
+++ myvue/src/components/test.vue
@@ -0,0 +1,23 @@
+<template>
+  <div id="app">
+    <appTable></appTable>
+  </div>
+</template>
+
+<script>
+import appTable from './table.vue'
+export default {
+  name: 'App',
+  data () {
+    tableTitles: ['测试id', '测试名称', '测试的其他数据'],
+    router: '/getUserInfo.do'
+  },
+  components: {
+    appTable
+  }
+}
+</script>
+
+<style>
+
+</style>
