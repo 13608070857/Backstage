287fe2de2239c906baef4b5b83fcea3b07702b6e
17711011036 <37230970+17711011036@users.noreply.github.com>
2018-07-20 16:54:44 +0800

    lin

---
+++ myvue/src/components/LeaseTransfer/leaseFrameBox.vue
@@ -0,0 +1,25 @@
+<template>
+    <figure>
+      <img :src="imgsrc" alt="">
+      <figcaption>
+        <p>商品编号:{{goodsID}}</p>
+        <p>商品名称:{{goodsName}}</p>
+        <p>商品价格:{{goodsPrice}}</p>
+        <input type="checkbox">
+      </figcaption>
+    </figure>
+</template>
+
+<script>
+    export default {
+        name: "leaseFrameBox",
+      data(){
+          return{}
+      },
+      props:['imgsrc','goodsID','goodsName','goodsPrice']
+    }
+</script>
+
+<style scoped>
+
+</style>
