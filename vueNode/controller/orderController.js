96e193654459559934a77fee1e20259961bf8fa0
YanChunlin96 <1519780292@qq.com>
2018-07-20 11:42:25 +0800

    交易记录

--- vueNode/controller/orderController.js
+++ vueNode/controller/orderController.js
@@ -11,27 +11,51 @@ const orderControoller = {
     },
     // 近一周
     orderweek(req,resp){
-
+        orderDao.ordercord()
+            .then(function (data) {
+                console.log(data);
+                resp.send(data);
+            })
     },
     // 近一个月
     orderyear1(req,resp){
-
+        orderDao.ordercordyear1()
+            .then(function (data) {
+                console.log(data);
+                resp.send(data);
+            })
     },
     // 近两个月
     orderyear2(req,resp){
-
+        orderDao.ordercordyear2()
+            .then(function (data) {
+                console.log(data);
+                resp.send(data);
+            })
     },
     // 近三个月
     orderyear3(req,resp){
-
+        orderDao.ordercordyear3()
+            .then(function (data) {
+                console.log(data);
+                resp.send(data);
+            })
     },
     // 今年
     orderthisyear(req,resp){
-
+        orderDao.ordercordthisyear()
+            .then(function (data) {
+                console.log(data);
+                resp.send(data);
+            })
     },
     // 去年
     orderlastyear(req,resp){
-
+        orderDao.ordercordlastyear()
+            .then(function (data) {
+                console.log(data);
+                resp.send(data);
+            })
     }
 };
 module.exports = orderControoller;
