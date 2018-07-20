0955f7bff1efa192af24abe56032c8a609ca8848
YanChunlin96 <1519780292@qq.com>
2018-07-20 17:30:27 +0800

    路由

--- vueNode/routes/manageRouter.js
+++ vueNode/routes/manageRouter.js
@@ -3,11 +3,25 @@ const router = express.Router();
 
 const userController = require("../controller/userController");
 const indexController = require("../controller/indexController");
+const orderController = require("../controller/orderController");
 
 
 router.get("/getUserInfo.do",userController.getUserInfo);
 
 router.get("/index",indexController.index);
 
+// =============== 交易管理 =============
+router.get("/orderrecord.do",orderController.orderrecord);
+router.get("/orderweek.do",orderController.orderweek);
+router.get("/orderyear1.do",orderController.orderyear1);
+router.get("/orderyear2.do",orderController.orderyear2);
+router.get("/orderyear3.do",orderController.orderyear3);
+router.get("/orderthisyear.do",orderController.orderthisyear);
+router.get("/orderlastyear.do",orderController.orderlastyear);
+
+// ================ 租赁管理 =============
+router.get("/LeaseTransfer.do",(req,resp)=>{
+
+});
 
 module.exports = router;
