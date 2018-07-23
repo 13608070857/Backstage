610c95b34b92b09aafe78371748cd57d50ed898d
Xiexinming <30278740+Xiexinming@users.noreply.github.com>
2018-07-23 09:35:39 +0800

    123

---
+++ myvue/src/components/index/Newest.vue
@@ -0,0 +1,49 @@
+<template>
+  <!--最新上架-->
+  <div class="Newest">
+    <h3>最新上架</h3>
+    <div class="goods"></div>
+    <div class="goods"></div>
+    <div class="goods"></div>
+    <div class="goods"></div>
+    <div class="goods"></div>
+    <div class="goods"></div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'newest',
+  data () {
+  },
+  created () {
+    // 最新商品
+    this.$axios.get('http://172.16.8.2:8888/Newest')
+      .then(resp => {
+        console.log(resp.data)
+      })
+  }
+}
+</script>
+
+<style scoped>
+.Newest{
+    width: 70%;
+    height: 500px;
+    float: left;
+    background: aquamarine;
+    margin-top: 30px;
+  }
+.Newest h3{
+    color: rgb(51,51,51);
+    text-indent: 1em;
+  }
+.goods{
+    width: 30%;
+    height: 200px;
+    background: aqua;
+    float: left;
+    margin-top: 20px;
+    margin-left: 20px;
+  }
+</style>
--- vueNode/routes/manageRouter.js
+++ vueNode/routes/manageRouter.js
@@ -8,6 +8,8 @@ const indexController = require("../controller/indexController");
 router.get("/getUserInfo.do",userController.getUserInfo);
 
 router.get("/index",indexController.index);
+router.get("/Newest",indexController.Newest);
+router.get("/xf",indexController.xf);
 
 
 module.exports = router;
