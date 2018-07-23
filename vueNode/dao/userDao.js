23412339e84114e373fc4c4d4e77d89a658e8462
jysson <jysson1224@users.noreply.github.com>
2018-07-23 17:27:57 +0800

    s

--- vueNode/dao/userDao.js
+++ vueNode/dao/userDao.js
@@ -1,9 +1,16 @@
 const db = require("../config/dbpoolConfig");
 
 const userDao = {
-    getUser(params) {
+    getAllUser(params) {
         return new Promise(function(resolve,reject) {
             db.connect("select * from users",[],function(error,data) {
+                    resolve(data);
+                });
+        });
+    },
+    getUser(params) {
+    	return new Promise(function(resolve,reject) {
+            db.connect("select u_id,name,tel,email,userImg,DATE_FORMAT(createTime,\"%Y-%m-%d %H:%i:%S\") from users",[],function(error,data) {
                     resolve(data);
                 });
         });
