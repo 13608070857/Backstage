58b5f3a7b0cc9bfad9dcab31b4b2961d6506dc5c
17711011036 <37230970+17711011036@users.noreply.github.com>
2018-07-25 15:03:44 +0800

    lin

--- vueNode/controller/leaseController.js
+++ vueNode/controller/leaseController.js
@@ -5,18 +5,14 @@ const leaseController = {
         leaseDao.leaseInquiry().then(function(data) {
             let getAllData = data;
             console.log(data);
-            dataInfo = {
-                getAllData: getAllData,
-            }
-            resp.send(dataInfo)
-            // leaseDao.leaseInquiry().then(function(data) {
-            //     let getData = data;
-            //     dataInfo = {
-            //         getAllData: getAllData,
-            //         getData: getData
-            //     };
-            //     resp.send(dataInfo);
-            // })
+            leaseDao.leaseInquiry().then(function(data) {
+                let getData = data;
+                dataInfo = {
+                    getAllData: getAllData,
+                    getData: getData
+                };
+                resp.send(dataInfo);
+            })
         });
     },
 }
