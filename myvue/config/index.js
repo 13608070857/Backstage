f45531e61406dbc562bbb30f7dc3e6c0f702e261
Xiexinming <30278740+Xiexinming@users.noreply.github.com>
2018-07-26 09:05:07 +0800

    123

--- myvue/config/index.js
+++ myvue/config/index.js
@@ -12,11 +12,7 @@ module.exports = {
     assetsPublicPath: '/',
     proxyTable: {
       '/api': {
-<<<<<<< HEAD
-        target: 'http://172.16.8.7:8888',
-=======
-        target: 'http://172.16.8.8:8888',
->>>>>>> 732462b00457bc655e05c901393faa8e6d0424cc
+        target: 'http://172.16.8.40:8888',
         changeOrigin: true,
         pathRewrite: {
           '^/api': ''
