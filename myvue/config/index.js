ec9072122eb279fb94e3ceaebfc7ff1d7f3d3815
YanChunlin96 <1519780292@qq.com>
2018-07-26 17:00:21 +0800

    ycl index

--- myvue/config/index.js
+++ myvue/config/index.js
@@ -19,11 +19,7 @@ module.exports = {
         }
       },
       '/xxm': {
-<<<<<<< HEAD
-        target: 'http://172.16.8.28:8888',
-=======
         target: 'http://172.16.8.8:8888',
->>>>>>> 21559cd7426afc43f43fc11e0f4ff201b0b31f09
         changeOrigin: true,
         pathRewrite: {
           '^/xxm': ''
